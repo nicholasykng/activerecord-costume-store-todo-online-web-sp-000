ba2190ce414bca27b4369de25e2189b2c21876a2
Nicholas Ng <nicholasykng@gmail.com>
2019-12-01 04:53:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.text :datetime
-      t.text :datetime
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
