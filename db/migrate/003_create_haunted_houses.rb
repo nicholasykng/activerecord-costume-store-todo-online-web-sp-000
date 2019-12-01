adebc659817a9de2487eca44b28127412e3a5022
Nicholas Ng <nicholasykng@gmail.com>
2019-12-01 04:59:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
-  
+  def change
+    create_table :haunted_houses do |t|
+      t.text :name
+      t.text :location
+      t.text :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :long_description
+    end
+  end
+end
