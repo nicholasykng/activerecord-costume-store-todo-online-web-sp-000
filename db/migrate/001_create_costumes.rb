741b96cd4d37189cd234e0ae4d1eea7fb22eda16
Nicholas Ng <nicholasykng@gmail.com>
2019-12-01 04:40:01 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,8 +1,9 @@
 class CreateCostumes < ActiveRecord::Migration[5.2]
-  create_table :costumes do |t|
-    t.text :name
-    t.text :price
-    t.text :size
-    t.text :image_url
+  def change
+    create_table :costumes do |t|
+      t.text :name
+      t.text :price
+      t.text :size
+      t.text :image_url
+    end
   end
-end
