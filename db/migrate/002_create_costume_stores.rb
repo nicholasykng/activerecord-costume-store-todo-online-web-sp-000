cb7d274280884ff308020888ae1a764cff5b8d48
Nicholas Ng <nicholasykng@gmail.com>
2019-12-01 04:50:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.text :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :employee_count
+      t.boolean :still_in_business
+      t.text :opening_time
+      t.text :closing_time
+    end
+  end
+end
